b9ecead8cf63cbccf4253de18c9f3a0e6a098702
Alvy <alvyahmed03@gmail.com>
2025-03-25 12:58:36 +0600

    Swagger ui v2

--- server.js
+++ server.js
@@ -15,33 +15,33 @@ const port = process.env.PORT || 3001;
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-  const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Library API",
-        version: "1.0.0",
-        description: "A simple Express Library API",
-      servers: [
-        {
-          url: "https://backendassignment-beta.vercel.app/",
-          description: "My API Documentation",
-        },
-      ],
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT",
-          },
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    servers: [
+      {
+        url: "https://backendassignment-beta.vercel.app/",
+        description: "My API Documentation",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
-      security: [{ BearerAuth: [] }],
     },
-    // This is to call all the file
-    apis: ["./api/routes/authRoutes.js","./api/routes/todosRoutes.js"],
-  }};
+    security: [{ BearerAuth: [] }],
+  },
+  // This is to call all the file
+  apis: ["./api/routes/authRoutes.js","./api/routes/todosRoutes.js"],
+}};
 
 
 // Middlewares
@@ -49,7 +49,11 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use("/todos", todoRoutes);
 app.use("/auth", authRoutes);
-app.use("/api", swaggerUi.serve, swaggerUi.setup(specs, { customCssUrl: CSS_URL }));
+app.use(
+  "/api",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
 
 
 app.get('/', (req, res) => {
